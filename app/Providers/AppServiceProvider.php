91c5739328432f2d393e23231cf34e17b81f5e1f
lukarrio1 <tomennis1997@gmail.com>
2025-08-08 12:20:21 -0500

    added the todos table

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -30,10 +30,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if ($this->app->environment('local')) {
-            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
-            $this->app->register(TelescopeServiceProvider::class);
-        }
+        // if ($this->app->environment('local')) {
+        //     $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+        //     $this->app->register(TelescopeServiceProvider::class);
+        // }
 
         $cache_driver = Setting::where('key', 'cache_driver')->first();
         if (!empty($cache_driver)) {
@@ -94,24 +94,24 @@ class AppServiceProvider extends ServiceProvider
             Cache::set('roles', Role::all()->pluck('id', 'name'));
         }
 
-        if (!Cache::has('setting_allowed_login_roles')) {
-            $allowed_login_roles = \getSetting("allowed_login_roles") ?? \collect([]);
-            Cache::add('setting_allowed_login_roles', $allowed_login_roles->toArray());
-        }
+        // if (!Cache::has('setting_allowed_login_roles')) {
+        //     $allowed_login_roles = \getSetting("allowed_login_roles") ?? \collect([]);
+        //     Cache::add('setting_allowed_login_roles', $allowed_login_roles->toArray());
+        // }
 
-        if (!Cache::has('setting_databases')) {
-            $databases = collect([]);
-            $data_configurations = \getSetting('database_configuration') ?? [];
-            collect($data_configurations)
-                ->keys()
-                ->each(fn($db) => $databases->put($db, $db));
-            Cache::add('setting_databases', $databases);
-        }
+        // if (!Cache::has('setting_databases')) {
+        //     $databases = collect([]);
+        //     $data_configurations = \getSetting('database_configuration') ?? [];
+        //     collect($data_configurations)
+        //         ->keys()
+        //         ->each(fn($db) => $databases->put($db, $db));
+        //     Cache::add('setting_databases', $databases);
+        // }
 
-        if (!Cache::has('setting_backup_databases')) {
-            $item = \getSetting('database_backup_configuration') ?? [];
-            Cache::add('setting_backup_databases', $item);
-        }
+        // if (!Cache::has('setting_backup_databases')) {
+        //     $item = \getSetting('database_backup_configuration') ?? [];
+        //     Cache::add('setting_backup_databases', $item);
+        // }
 
         if (!Cache::has('routes')) {
             $nodes = Node::enabled()
@@ -120,58 +120,58 @@ class AppServiceProvider extends ServiceProvider
             Cache::add('routes', $nodes);
         }
 
-        if (!Cache::has('references')) {
-            Cache::add('references', ReferenceConfig::query()
-                ->whereIn('type', \getSetting('reference_types') ?? collect([]))
-                ->distinct('type')
-                ->get());
-        }
+        // if (!Cache::has('references')) {
+        //     Cache::add('references', ReferenceConfig::query()
+        //         ->whereIn('type', \getSetting('reference_types') ?? collect([]))
+        //         ->distinct('type')
+        //         ->get());
+        // }
 
-        \collect(\getSetting('reference_types'))
-            ->each(function ($ref) {
-                $rel_type = collect(\explode('_', $ref));
-                $ref = Cache::get('references')->where('type', $ref)->first();
-                if ($rel_type->count() > 1 && !empty($ref)) {
-                    $owned_model = $ref->owned_model;
-                    $owner_model = $ref->owner_model;
-                    $has_many = (int) $rel_type->last() == 1 ? "hasManyThrough" : "hasOneThrough";
+        // \collect(\getSetting('reference_types'))
+        //     ->each(function ($ref) {
+        //         $rel_type = collect(\explode('_', $ref));
+        //         $ref = Cache::get('references')->where('type', $ref)->first();
+        //         if ($rel_type->count() > 1 && !empty($ref)) {
+        //             $owned_model = $ref->owned_model;
+        //             $owner_model = $ref->owner_model;
+        //             $has_many = (int) $rel_type->last() == 1 ? "hasManyThrough" : "hasOneThrough";
 
-                    // creates the owner relationships
-                    $owner_model::resolveRelationUsing($rel_type->first(), function ($owner_model) use ($owned_model, $has_many, $ref) {
-                        return $owner_model->$has_many($owned_model, Reference::class, 'owner_id', 'id', 'id', 'owned_id')
-                            ->where('references.type', $ref->type);
-                    });
-                    // creates the reverse of the owned relationship
-                    $owned_model::resolveRelationUsing($rel_type->first() . "_owner", function ($owned_model) use ($owner_model, $has_many, $ref) {
-                        return $owned_model->hasOneThrough($owner_model, Reference::class, 'owned_id', 'id', 'id', 'owner_id')
-                            ->where('references.type', $ref->type);
-                    });
-                }
-            });
+        //             // creates the owner relationships
+        //             $owner_model::resolveRelationUsing($rel_type->first(), function ($owner_model) use ($owned_model, $has_many, $ref) {
+        //                 return $owner_model->$has_many($owned_model, Reference::class, 'owner_id', 'id', 'id', 'owned_id')
+        //                     ->where('references.type', $ref->type);
+        //             });
+        //             // creates the reverse of the owned relationship
+        //             $owned_model::resolveRelationUsing($rel_type->first() . "_owner", function ($owned_model) use ($owner_model, $has_many, $ref) {
+        //                 return $owned_model->hasOneThrough($owner_model, Reference::class, 'owned_id', 'id', 'id', 'owner_id')
+        //                     ->where('references.type', $ref->type);
+        //             });
+        //         }
+        //     });
 
-        \collect(
-            \getSetting('database_configuration')
-        )->filter(function ($item) {
-            return !empty($item->get('DB_CONNECTION') ?? '');
-        })
-            ->each(function ($item, $key) {
-                try {
-                    Config::set("database.connections.{$key}", [
-                        'driver' => $item->get('DB_CONNECTION') ?? "mysql",
-                        'host' => $item->get('DB_HOST'),
-                        'port' => $item->get('DB_PORT'),
-                        'database' => $item->get('DB_DATABASE'),
-                        'username' => $item->get('DB_USERNAME'),
-                        'password' => $item->get('DB_PASSWORD'),
-                        'charset' => 'utf8',
-                        'collation' => 'utf8_unicode_ci',
-                        'prefix' => '',
-                    ]);
-                } catch (\Throwable $th) {
-                    //throw $th;
-                }
-            });
-        (new User())->deleteInactiveUsers();
+        // \collect(
+        //     \getSetting('database_configuration')
+        // )->filter(function ($item) {
+        //     return !empty($item->get('DB_CONNECTION') ?? '');
+        // })
+        //     ->each(function ($item, $key) {
+        //         try {
+        //             Config::set("database.connections.{$key}", [
+        //                 'driver' => $item->get('DB_CONNECTION') ?? "mysql",
+        //                 'host' => $item->get('DB_HOST'),
+        //                 'port' => $item->get('DB_PORT'),
+        //                 'database' => $item->get('DB_DATABASE'),
+        //                 'username' => $item->get('DB_USERNAME'),
+        //                 'password' => $item->get('DB_PASSWORD'),
+        //                 'charset' => 'utf8',
+        //                 'collation' => 'utf8_unicode_ci',
+        //                 'prefix' => '',
+        //             ]);
+        //         } catch (\Throwable $th) {
+        //             //throw $th;
+        //         }
+        //     });
+        // (new User())->deleteInactiveUsers();
     }
 
     public function boot1()
